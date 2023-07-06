lowRISC/qemu

[ot] target/riscv: update lowrisc-ibex core initialization
Define a generic version of lowrisc-ibex core that can be used in several machines:

 - leave MISA empty so that generic properties can be defined for this core
 - remove all arbitrary default properties but ISA I,C,U which are mandatory for ibex
 - define default mtvec which is only support vectored mode
 - update privilege version (1.12) according to the Ibex documentation
 - define ibex architecture identifier

Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

target/riscv/cpu.c
@@ -486,14 +486,37 @@ static void rv32_lowrisc_ibex_cpu_init(Object *obj)
     CPURISCVState *env = &RISCV_CPU(obj)->env;
     RISCVCPU *cpu = RISCV_CPU(obj);
 
-    set_misa(env, MXL_RV32, RVI | RVM | RVC | RVU);
+    set_misa(env, MXL_RV32, 0);
     register_cpu_props(obj);
-    set_priv_version(env, PRIV_VERSION_1_11_0);
+    set_priv_version(env, PRIV_VERSION_1_12_0);
+    /*
+     * QEMU RISC-V implementation defines a list of by-default enabled extensions that
+     * need to be disabled.
+     */
+    cpu->cfg.ext_m = false;
+    cpu->cfg.ext_a = false;
+    cpu->cfg.ext_f = false;
+    cpu->cfg.ext_d = false;
+    cpu->cfg.ext_s = false;
+    cpu->cfg.ext_h = false;
+    cpu->cfg.ext_zawrs = false;
+    cpu->cfg.ext_zihintpause = false;
     cpu->cfg.mmu = false;
+    cpu->cfg.pmp = false;
+    cpu->cfg.ext_sstc = false;
+    cpu->cfg.ext_svadu = false;
+    cpu->cfg.pmu_num = 0;
+    cpu->cfg.ext_zba = false;
+    cpu->cfg.ext_zbb = false;
+    cpu->cfg.ext_zbc = false;
+    cpu->cfg.ext_zbs = false;
+    cpu->cfg.ext_zicbom = false;
+    cpu->cfg.ext_zicboz = false;
+    cpu->cfg.marchid = 0x16u;
+    cpu->cfg.mtvec = 0x00000001u;
 #ifndef CONFIG_USER_ONLY
     set_satp_mode_max_supported(cpu, VM_1_10_MBARE);
 #endif
-    cpu->cfg.epmp = true;
 }
 
 static void rv32_imafcu_nommu_cpu_init(Object *obj)

pamaury/qemu-ot

target/riscv/cpu.c
@@ -486,14 +486,37 @@ static void rv32_lowrisc_ibex_cpu_init(Object *obj)
     CPURISCVState *env = &RISCV_CPU(obj)->env;
     RISCVCPU *cpu = RISCV_CPU(obj);
 
-    set_misa(env, MXL_RV32, RVI | RVM | RVC | RVU);
+    set_misa(env, MXL_RV32, 0);
     register_cpu_props(obj);
-    set_priv_version(env, PRIV_VERSION_1_11_0);
+    set_priv_version(env, PRIV_VERSION_1_12_0);
+    /*
+     * QEMU RISC-V implementation defines a list of by-default enabled extensions that
+     * need to be disabled.
+     */
+    cpu->cfg.ext_m = false;
+    cpu->cfg.ext_a = false;
+    cpu->cfg.ext_f = false;
+    cpu->cfg.ext_d = false;
+    cpu->cfg.ext_s = false;
+    cpu->cfg.ext_h = false;
+    cpu->cfg.ext_zawrs = false;
+    cpu->cfg.ext_zihintpause = false;
     cpu->cfg.mmu = false;
+    cpu->cfg.pmp = false;
+    cpu->cfg.ext_sstc = false;
+    cpu->cfg.ext_svadu = false;
+    cpu->cfg.pmu_num = 0;
+    cpu->cfg.ext_zba = false;
+    cpu->cfg.ext_zbb = false;
+    cpu->cfg.ext_zbc = false;
+    cpu->cfg.ext_zbs = false;
+    cpu->cfg.ext_icbom = false;
+    cpu->cfg.ext_icboz = false;
+    cpu->cfg.marchid = 0x16u;
+    cpu->cfg.mtvec = 0x00000001u;
 #ifndef CONFIG_USER_ONLY
     set_satp_mode_max_supported(cpu, VM_1_10_MBARE);
 #endif
-    cpu->cfg.epmp = true;
 }
 
 static void rv32_imafcu_nommu_cpu_init(Object *obj)